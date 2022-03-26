95a46e9cdef35ff9e418116b449742bedab9089b
Abelardo <matheusabelardo12@hotmail.com>
2022-03-26 19:21:00 -0300

    IA do inimigos
    Fazendo os inimigos irem atras do player

--- src/Abelardo/entities/Inimigo.java
+++ src/Abelardo/entities/Inimigo.java
@@ -2,10 +2,30 @@ package Abelardo.entities;
 
 import java.awt.image.BufferedImage;
 
+import Abelardo.word.Word;
+import Game.Game;
+
 public class Inimigo extends Entity {
+	
+	private double spd = 1;
 
 	public Inimigo(int x, int y, int width, int heigth, BufferedImage sprite) {
 		super(x, y, width, heigth, sprite);
+	}
+	
+	public void tick() {
+		
+		if((int)x < Game.player.getX() && Word.isFree((int)(x+spd), this.getY())) {
+			x+=spd;
+		}if((int)x > Game.player.getX()&& Word.isFree((int)(x-spd), this.getY())) {
+			x-=spd;
+		}if((int)y < Game.player.getY()&& Word.isFree(this.getX(), (int)(y+spd))) {
+			y+=spd;
+		}if((int)y > Game.player.getY()&& Word.isFree(this.getX(), (int)(y-spd))) {
+			y-=spd;
+		}
+		
+		
 	}
 
 }
